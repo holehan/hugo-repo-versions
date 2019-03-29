303a1fb06e90f510dc6f723ac1e9570472e01323
Florian Grässle <hallo@holehan.org>
2019-03-29 11:39:02 +0100

    Use hugo's inbuilt getJSON to get json files

--- content/snap.md
+++ content/snap.md
@@ -1,6 +1,8 @@
 ---
 title: 'Snap'
 repo: snap
+jsonUrlPre: 'https://search.apps.ubuntu.com'
+jsonUrl: '/api/v1/search?q=hugo'
 outputs:
   - version-badge
 ---
--- package-scripts.js
+++ package-scripts.js
@@ -3,16 +3,10 @@ module.exports = {
     default: 'hugo',
     clean: 'rm -rf public resources data/* docker.txt',
     updateRepos: {
-      copr:
-        'curl -s "https://copr.fedorainfracloud.org/api_2/builds?project_id=12493&limit=1" > data/copr.json',
-      snap:
-        'curl -s "https://search.apps.ubuntu.com/api/v1/search?q=hugo" > data/snap.json',
-      github:
-        'curl -s "https://api.github.com/repos/gohugoio/hugo/releases/latest" > data/github.json',
       docker:
         'curl -a "https://raw.githubusercontent.com/cibuilds/hugo/master/build-images.sh" > docker.txt'
     },
-    build: 'nps clean && nps u.c u.s u.g u.d && hugo --minify --gc',
+    build: 'nps clean && nps u.d && hugo --minify --gc',
     watch: 'nps build && hugo server'
   }
 };
