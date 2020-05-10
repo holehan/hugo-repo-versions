98e9c4f30d21f8b7ef738d236babffa683ce732f
Florian Grässle <florian@graessle.com>
2020-05-10 14:21:32 +0200

    Rebuild on hugo updates
    - Adjust update check to check for new hugo release and trigger rebuild
    - Switch to netlify.app-domain
    - Extract hugo version from tag_name instead of name as name could be any
      fancy string bep thinks cool

--- check-for-updates.sh
+++ check-for-updates.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+HUGO_REMOTE="$(curl -s 'https://api.github.com/repos/gohugoio/hugo/releases/latest' | jq -r '.tag_name')"
+HUGO_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/hugo/index.json | jq -r '.tag_name')"
 SNAP_REMOTE="$(curl -s -H 'X-Ubuntu-Series: 16' 'https://api.snapcraft.io/api/v1/snaps/details/hugo' | jq -r '.version')"
-SNAP_LOCAL="$(curl -s https://hugo-repo-versions.netlify.com/snap/index.json | jq -r '.data.version')"
+SNAP_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/snap/index.json | jq -r '.data.version')"
 SNAP_EXTENDED_REMOTE="$(curl -s -H 'X-Ubuntu-Series: 16' 'https://api.snapcraft.io/api/v1/snaps/details/hugo?channel=extended' | jq -r '.version')"
-SNAP_EXTENDED_LOCAL="$(curl -s https://hugo-repo-versions.netlify.com/snap-extended/index.json | jq -r '.data.version')"
+SNAP_EXTENDED_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/snap-extended/index.json | jq -r '.data.version')"
 DOCKER_REMOTE="$(curl -s 'https://raw.githubusercontent.com/cibuilds/hugo/master/build-images.sh' | grep -Eo -m 1 'hugo:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sed 's/^hugo://g')"
-DOCKER_LOCAL="$(curl -s https://hugo-repo-versions.netlify.com/docker/index.json | jq -r '.data.version')"
+DOCKER_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/docker/index.json | jq -r '.data.version')"
 MINIMALDOCKER_REMOTE="$(curl -s https://raw.githubusercontent.com/klakegg/docker-hugo/master/Dockerfile-base | grep -m 1 'ARG VERSION=' | sed 's/^ARG VERSION=//g')"
-MINIMALDOCKER_LOCAL="$(curl -s https://hugo-repo-versions.netlify.com/minimaldocker/index.json | jq -r '.data.version')"
+MINIMALDOCKER_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/minimaldocker/index.json | jq -r '.data.version')"
 COPR_REMOTE="$(curl -s 'https://copr.fedorainfracloud.org/api_2/builds?project_id=12493&limit=1' | jq -r '.builds[0].build.built_packages[0].version')"
-COPR_LOCAL="$(curl -s https://hugo-repo-versions.netlify.com/copr/index.json | jq -r '.data.version')"
+COPR_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/copr/index.json | jq -r '.data.version')"
 
+# echo $HUGO_REMOTE
+# echo $HUGO_LOCAL
 # echo $SNAP_REMOTE
 # echo $SNAP_LOCAL
 # echo $SNAP_EXTENDED_REMOTE
@@ -22,11 +26,13 @@ COPR_LOCAL="$(curl -s https://hugo-repo-versions.netlify.com/copr/index.json | j
 # echo $COPR_REMOTE
 # echo $COPR_LOCAL
 
-if [ $SNAP_REMOTE != $SNAP_LOCAL ] ||
+if [ $HUGO_REMOTE != $HUGO_LOCAL ] ||
+  [ $SNAP_REMOTE != $SNAP_LOCAL ] ||
   [ $SNAP_EXTENDED_REMOTE != $SNAP_EXTENDED_LOCAL ] ||
   [ $DOCKER_REMOTE != $DOCKER_LOCAL ] ||
   [ $MINIMALDOCKER_REMOTE != $MINIMALDOCKER_LOCAL ] ||
   [ $COPR_REMOTE != $COPR_LOCAL ]; then
   echo "Repo info needs updating. Triggering netlify rebuild."
-  exit 1
+  exit 0
 fi
+exit 1
