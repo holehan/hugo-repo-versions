b45161ff64b6ffd8bf259176dccd336eab7af827
Florian Grässle <florian@graessle.com>
2022-09-03 14:26:18 +0200

    Fix snap version lookup

--- check-for-updates.sh
+++ check-for-updates.sh
@@ -2,9 +2,9 @@
 
 HUGO_REMOTE="$(curl -s 'https://api.github.com/repos/gohugoio/hugo/releases/latest' | jq -r '.tag_name' | sed 's/^v//g')"
 HUGO_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/hugo/index.json | jq -r '.tag_name' | sed 's/^v//g')"
-SNAP_REMOTE="$(curl -s -H 'X-Ubuntu-Series: 16' 'https://api.snapcraft.io/api/v1/snaps/details/hugo' | jq -r '.version' | grep -Eo -m 1 'v[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sed 's/^v//g')"
+SNAP_REMOTE="$(curl -s -H 'X-Ubuntu-Series: 16' 'https://api.snapcraft.io/api/v1/snaps/details/hugo' | jq -r '.version' | grep -Eo -m 1 '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sed 's/^v//g')"
 SNAP_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/snap/index.json | jq -r '.data.version')"
-SNAP_EXTENDED_REMOTE="$(curl -s -H 'X-Ubuntu-Series: 16' 'https://api.snapcraft.io/api/v1/snaps/details/hugo?channel=extended' | jq -r '.version' | grep -Eo -m 1 'v[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sed 's/^v//g')"
+SNAP_EXTENDED_REMOTE="$(curl -s -H 'X-Ubuntu-Series: 16' 'https://api.snapcraft.io/api/v1/snaps/details/hugo?channel=extended' | jq -r '.version' | grep -Eo -m 1 '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sed 's/^v//g')"
 SNAP_EXTENDED_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/snap-extended/index.json | jq -r '.data.version')"
 DOCKER_REMOTE="$(curl -s 'https://raw.githubusercontent.com/cibuilds/hugo/master/build-images.sh' | grep -Eo -m 1 'hugo:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sed 's/^hugo://g')"
 DOCKER_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/docker/index.json | jq -r '.data.version')"
@@ -13,18 +13,18 @@ MINIMALDOCKER_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/minimaldoc
 COPR_REMOTE="$(curl -s 'https://copr.fedorainfracloud.org/api_2/builds?project_id=12493&limit=1' | jq -r '.builds[0].build.package_version' | grep -Eo -m 1 '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')"
 COPR_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/copr/index.json | jq -r '.data.version')"
 
-# echo $HUGO_REMOTE
-# echo $HUGO_LOCAL
-# echo $SNAP_REMOTE
-# echo $SNAP_LOCAL
-# echo $SNAP_EXTENDED_REMOTE
-# echo $SNAP_EXTENDED_LOCAL
-# echo $DOCKER_REMOTE
-# echo $DOCKER_LOCAL
-# echo $MINIMALDOCKER_REMOTE
-# echo $MINIMALDOCKER_LOCAL
-# echo $COPR_REMOTE
-# echo $COPR_LOCAL
+echo $HUGO_REMOTE
+echo $HUGO_LOCAL
+echo $SNAP_REMOTE
+echo $SNAP_LOCAL
+echo $SNAP_EXTENDED_REMOTE
+echo $SNAP_EXTENDED_LOCAL
+echo $DOCKER_REMOTE
+echo $DOCKER_LOCAL
+echo $MINIMALDOCKER_REMOTE
+echo $MINIMALDOCKER_LOCAL
+echo $COPR_REMOTE
+echo $COPR_LOCAL
 
 if [ $HUGO_REMOTE != $HUGO_LOCAL ] ||
   [ $SNAP_REMOTE != $SNAP_LOCAL ] ||
