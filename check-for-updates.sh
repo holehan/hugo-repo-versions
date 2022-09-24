f77746b1e39a806e6526cffac4f7dcced447d205
Florian Grässle <florian@graessle.com>
2022-09-24 15:40:48 +0200

    Fix copr

--- check-for-updates.sh
+++ check-for-updates.sh
@@ -10,21 +10,21 @@ DOCKER_REMOTE="$(curl -s 'https://raw.githubusercontent.com/cibuilds/hugo/master
 DOCKER_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/docker/index.json | jq -r '.data.version')"
 MINIMALDOCKER_REMOTE="$(curl -s 'https://api.github.com/repos/klakegg/docker-hugo/releases/latest' | jq -r '.tag_name')"
 MINIMALDOCKER_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/minimaldocker/index.json | jq -r '.data.version')"
-COPR_REMOTE="$(curl -s 'https://copr.fedorainfracloud.org/api_2/builds?project_id=12493&limit=1' | jq -r '.builds[0].build.package_version' | grep -Eo -m 1 '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')"
+COPR_REMOTE="$(curl -s 'https://copr.fedorainfracloud.org/api_3/build/list/?ownername=daftaupe&projectname=hugo&limit=1' | jq -r '.items[0].source_package.version' | grep -Eo -m 1 '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')"
 COPR_LOCAL="$(curl -s https://hugo-repo-versions.netlify.app/copr/index.json | jq -r '.data.version')"
 
-echo $HUGO_REMOTE
-echo $HUGO_LOCAL
-echo $SNAP_REMOTE
-echo $SNAP_LOCAL
-echo $SNAP_EXTENDED_REMOTE
-echo $SNAP_EXTENDED_LOCAL
-echo $DOCKER_REMOTE
-echo $DOCKER_LOCAL
-echo $MINIMALDOCKER_REMOTE
-echo $MINIMALDOCKER_LOCAL
-echo $COPR_REMOTE
-echo $COPR_LOCAL
+echo "Hugo remote $HUGO_REMOTE"
+echo "Hugo local $HUGO_LOCAL"
+echo "Snap remote $SNAP_REMOTE"
+echo "Snap local $SNAP_LOCAL"
+echo "Snap extended remote $SNAP_EXTENDED_REMOTE"
+echo "Snap extended local $SNAP_EXTENDED_LOCAL"
+echo "Docker remote $DOCKER_REMOTE"
+echo "Docker local $DOCKER_LOCAL"
+echo "Minimal docker remote $MINIMALDOCKER_REMOTE"
+echo "Minimal docker local $MINIMALDOCKER_LOCAL"
+echo "COPR remote $COPR_REMOTE"
+echo "COPR local $COPR_LOCAL"
 
 if [ $HUGO_REMOTE != $HUGO_LOCAL ] ||
   [ $SNAP_REMOTE != $SNAP_LOCAL ] ||
--- content/copr.md
+++ content/copr.md
@@ -2,7 +2,7 @@
 title: "Copr"
 repo: copr
 jsonUrlPre: "https://copr.fedorainfracloud.org"
-jsonUrl: "/api_2/builds?project_id=12493&limit=1"
+jsonUrl: "/api_3/build/list/?ownername=daftaupe&projectname=hugo&limit=1"
 outputs:
   - version-badge
   - json
